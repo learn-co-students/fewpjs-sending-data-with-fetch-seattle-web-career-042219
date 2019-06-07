77f082108d00bb944e078896a69c19ffa3967f51
github username <github email address>
2019-06-06 17:53:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const URL = "http://localhost:3000/users"
+
+function main() {
+	submitData();
+}
+main();
+
+function submitData(name, email) {
+	return fetch(URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify({name, email})
+	})
+	.then(resp => resp.json())
+	.then(json => document.body.innerHTML = json["id"]) 
+	.catch(error => {
+		document.body.innerHTML = error.message
+	});
+}
