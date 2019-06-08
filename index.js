370eda16155c359b1b8dbf1ecbb4a59c30d3b76a
Milan <milan.dhar@gmail.com>
2019-06-07 18:05:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(res=> {
+    return res.json()
+  })
+  .then(json=> {
+    document.body.innerHTML = json[ "id" ]
+  })
+  .catch(err => {
+     document.body.innerHTML = err.message
+  });
+}
