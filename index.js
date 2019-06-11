07aef08a8453051e128529de3b9d724ed82f03da
austenhasty <hastyae@gmail.com>
2019-06-11 12:12:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+
+
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "Post",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(res => {
+    return res.json()
+  })
+  .then(json => {
+    document.body.innerHTML = json["id"]
+  })
+  .catch(error => {
+    document.body.innerHTML = error.message
+  })
+}
+  // })
+//   // return fetch('http://localhost:3000/users', {
+//   //   method: "Post",
+//   //   headers: {
+//   //     "Content-Type": "application/json",
+//   //     "Accept": "application/json"
+//   //   },
+//     .then(response => response.json())
+//     .then(json => {
+//       console.log(json)
+//     })
+//     // .catch(error => {
+//     // console.log(error.message)
+//     // })
+//   }
+// }
