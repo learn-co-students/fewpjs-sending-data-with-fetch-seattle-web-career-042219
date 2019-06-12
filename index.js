ffd71a221cb209f6c923a98d3175944f398ab564
github username <github email address>
2019-06-12 12:20:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then( res => res.json())
+  .then( json => {
+    let idDiv = document.createElement('div');
+    idDiv.textContent = json.id
+    document.body.appendChild(idDiv)
+    return json
+  })
+  .catch( err => {
+    let idDiv = document.createElement('div');
+    idDiv.textContent = "Unauthorized Access"
+    document.body.appendChild(idDiv)
+  })
+}
