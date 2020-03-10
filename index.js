0b69fdcea5a69ea8cb8e30f215b93dbe8dc67744
austenhasty <hastyae@gmail.com>
2020-03-09 22:56:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 // Add your code here
-
-
-function submitData(name, email) {
-  return fetch('http://localhost:3000/users', {
-    method: "Post",
+function submitData(userName, userEmail) {
+  let userData = {
+    name: userName,
+    email: userEmail
+  };
+  let config = {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
+      'Content-Type' : 'application/json',
+      'Accept': 'application/json'
     },
-    body: JSON.stringify({
-      name: name,
-      email: email
-    })
-  })
-  .then(res => {
-    return res.json()
-  })
-  .then(json => {
-    document.body.innerHTML = json["id"]
-  })
-  .catch(error => {
+    body: JSON.stringify(userData)
+  };
+  return fetch('http://localhost:3000/users', config)
+  .then(function(response) {
+    return response.json()
+  }) .then(function(object) {
+    document.body.innerHTML = object.id
+  }) .catch(function(error) {
     document.body.innerHTML = error.message
-  })
+  });
 }
